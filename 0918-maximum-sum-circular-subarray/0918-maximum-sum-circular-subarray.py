a071588df0b940d48a4eb44a8b311b874d1e80cf
Sam <46853557+sammcdo@users.noreply.github.com>
2023-03-02 17:17:55 -0500

    Time: 543 ms (60.87%), Space: 19.5 MB (8.82%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -1,40 +1,25 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        nums2 = nums + nums
-
-        t = [True if i < 0 else False for i in nums]
-
-        
         if len(nums) == 1:
             return nums[0]
         
-        maximum = self.maxSubArray(nums)
-        minimum = self.minSubArray(nums)
+        dp1 = [nums[0]]
+        dp2 = [nums[0]]
+        
+        hasPositive = False
+        for i in range(1, len(nums)):
+            a = min(dp1[i-1] + nums[i], nums[i])
+            dp1.append(a)
+            a = max(dp2[i-1] + nums[i], nums[i])
+            dp2.append(a)
+            
+            if nums[i] > 0:
+                hasPositive = True
+        
+        maximum = max(dp2)
+        minimum = min(dp1)
 
-        if False in t:
+        if hasPositive:
             return max(maximum, sum(nums) - minimum)
         else:
             return min(maximum, sum(nums) - minimum)
-    
-    def minSubArray(self,nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        
-        dp = [nums[0]]
-        for i in range(1,len(nums)):
-            a = min(dp[i-1] + nums[i], nums[i])
-            dp.append(a)
-        
-        return min(dp)
-        
-        
-    def maxSubArray(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        
-        dp = [nums[0]]
-        for i in range(1,len(nums)):
-            a = max(dp[i-1] + nums[i], nums[i])
-            dp.append(a)
-        
-        return max(dp)
