2c64b3a04acc84f1040d166e49c2336d5f6ef7ed
Sam <46853557+sammcdo@users.noreply.github.com>
2023-03-06 11:14:54 -0500

    Time: 446 ms (98.58%), Space: 20.6 MB (60.50%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -3,14 +3,9 @@ class Solution:
         if len(values) == 2:
             return values[0]+values[1] - 1
         
-        dp = [-1] * len(values)
-        
-        dp[0] = 0
-        dp[1] = values[0]+values[1] - 1
-        
         prevMax = max(values[0], values[1])
         prevMaxLoc = values.index(prevMax)
-        m = dp[1]
+        m = values[0]+values[1] - 1
         for i in range(2, len(values)):
             if prevMax + values[i] - i + prevMaxLoc >= m:
                 m = prevMax + values[i] - i + prevMaxLoc
